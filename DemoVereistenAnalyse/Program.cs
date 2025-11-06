8f51481a1ac2d7a5ae8d8defa789e2cc169c7dae
MateuszMajsterPXL <mateusz.majster@student.pxl.be>
2025-11-06 14:05:12 +0100

    DEMO-04: foutmelding

--- DemoVereistenAnalyse/Program.cs
+++ DemoVereistenAnalyse/Program.cs
@@ -7,12 +7,20 @@
             const string user = "Admin", password = "P@sswoord123";
             int tries = 0;
 
-            Console.Write("Gebruikersnaam: ");
-            string inputUser = Console.ReadLine();
-            Console.Write("Wachtwoord: ");
-            string inputPassword = Console.ReadLine();
+            while (tries < 3)
+            {
+                Console.Write("Gebruikersnaam: ");
+                string inputUser = Console.ReadLine();
+                Console.Write("Wachtwoord: ");
+                string inputPassword = Console.ReadLine();
 
-            if (inputUser == user && inputPassword == password) { }
+                if (inputUser == user && inputPassword == password)
+
+                tries++;
+                Console.WriteLine($"Foutieve login. \n{3 - tries} pogingen over. ");
+            }
+            Console.WriteLine("Te veel pogingen. Toegang geweigerd.");
+
         }
     }
 }
