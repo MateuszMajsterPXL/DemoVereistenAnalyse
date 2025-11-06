b442fc4729b2d51e5de47a53ed477b607c33e2e2
MateuszMajsterPXL <mateusz.majster@student.pxl.be>
2025-11-06 14:08:28 +0100

    DEMO-05: welkomstbericht

--- DemoVereistenAnalyse/Program.cs
+++ DemoVereistenAnalyse/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            const string user = "Admin", password = "P@sswoord123";
+            const string user = "Admin", password = "password";
             int tries = 0;
 
             while (tries < 3)
@@ -15,9 +15,13 @@
                 string inputPassword = Console.ReadLine();
 
                 if (inputUser == user && inputPassword == password)
+                {
+                    Console.WriteLine($"Welkom, {inputUser}!");
+                    return;
+                }
 
                 tries++;
-                Console.WriteLine($"Foutieve login. \n{3 - tries} pogingen over. ");
+                Console.WriteLine($"Foutieve login. \n{3-tries} pogingen over. ");
             }
             Console.WriteLine("Te veel pogingen. Toegang geweigerd.");
 
