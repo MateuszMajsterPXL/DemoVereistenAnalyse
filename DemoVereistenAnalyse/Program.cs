c137404b59d0486484c2d826de94221084ba5e35
MateuszMajsterPXL <mateusz.majster@student.pxl.be>
2025-11-06 13:58:49 +0100

    DEMO-03: inlogcontrole

--- DemoVereistenAnalyse/Program.cs
+++ DemoVereistenAnalyse/Program.cs
@@ -4,8 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.Write("Gebruikersnaam");
+            const string user = "Admin", password = "P@sswoord123";
+            int tries = 0;
+
+            Console.Write("Gebruikersnaam: ");
             string inputUser = Console.ReadLine();
+            Console.Write("Wachtwoord: ");
+            string inputPassword = Console.ReadLine();
+
+            if (inputUser == user && inputPassword == password) { }
         }
     }
 }
